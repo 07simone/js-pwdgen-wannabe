e81524bbc6ada848c7c0a2aad15a1569af013022
simone andreozzi <simoneandreozzi8@gmail.com>
2022-02-21 20:42:53 +0100

    modifiche-risultato-finale

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@ document.getElementById('my-surname') .innerHTML = surname;
 const color = prompt("Quale è il tuo colore preferito?");
 document.getElementById('my-favorite-color') .innerHTML = color;
 
-let sum = firstname + surname + color + 21;
+
+/*  let sum  = firstname + " " + surname + color + 21;  */
+let sum =`${firstname}${surname}${color}21`; 
 document.getElementById('my-password') .innerHTML = sum; 
-console.log(sum);
+console.log(`${firstname}${surname}${color}21`); 
+
