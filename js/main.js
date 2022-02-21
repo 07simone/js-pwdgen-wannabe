88d02d7f626a5af45d8c0cd66ff2476ec7d57201
simone andreozzi <simoneandreozzi8@gmail.com>
2022-02-21 16:49:04 +0100

    somma-degli-elementi-password

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const firstname = prompt("Come ti chiami?");
 document.getElementById('my-name') .innerHTML = firstname;
 
@@ -7,5 +8,6 @@ document.getElementById('my-surname') .innerHTML = surname;
 const color = prompt("Quale è il tuo colore preferito?");
 document.getElementById('my-favorite-color') .innerHTML = color;
 
-const password = firstname + surname + color + 21;
-document.getElementById('my-password') .innerHTML = password;
+let sum = firstname + surname + color + 21;
+document.getElementById('my-password') .innerHTML = sum; 
+console.log(sum);
